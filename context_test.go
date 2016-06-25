dd32940a1544d9e204e2b545429ab8cee64ba8e3
gomydodo <gomydodo@163.com>
2016-06-25 10:44:55 +0800

    add test for context

---
+++ context_test.go
@@ -0,0 +1,64 @@
+package wechat
+
+import (
+	"encoding/xml"
+	"github.com/gomydodo/wxencrypter"
+	"log"
+	"testing"
+)
+
+const (
+	token          = "pamtest"
+	appID          = "wxb11529c136998cb6"
+	encodingAesKey = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFG"
+)
+
+var w *Wechat
+
+func init() {
+	var err error
+	w, err = New(token, appID, encodingAesKey, true)
+	if err != nil {
+		log.Fatal("new wechat error :", err)
+	}
+}
+
+func TestEncrypt(t *testing.T) {
+	b, _ := xml.Marshal(NewTextResponseMessage("to", "from", "content"))
+
+	b, err := w.Encrypt(b)
+	if err != nil {
+		t.Fatal("wechat encrypt error: ", err)
+	}
+
+	e, err := wxencrypter.ParseResponseXML(b)
+	if err != nil {
+		t.Fatal("parse xml error :", err)
+	}
+
+	b, err = w.Decrypt(e.MsgSignature, e.TimeStamp, e.Nonce, b)
+	if err != nil {
+		t.Fatal("decrypt data error: ", err)
+	}
+
+}
+
+func TestNewTextResponseMessage(t *testing.T) {
+	tm := NewTextResponseMessage("to", "from", "content")
+
+	b, err := xml.Marshal(tm)
+	if err != nil {
+		t.Fatal("marshal text response message error: ", err)
+	}
+
+	dft := &defaultRequestMessage{}
+
+	err = dft.Unmarshal(b)
+	if err != nil {
+		t.Fatal("unmarshal request message error: ", err)
+	}
+
+	if dft.ToUserName() != "to" {
+		t.Fatalf("parse tousername erorr, should be %s, actual %s", "to", dft.ToUserName())
+	}
+}
